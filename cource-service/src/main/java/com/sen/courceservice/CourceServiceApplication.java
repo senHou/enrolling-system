49b02a88d6c795cf00a3897a4108fb75341e0d1f
shou <sen.hou1987@gamil.com>
2024-01-12 23:32:48 +1100

    add course service

---
+++ cource-service/src/main/java/com/sen/courceservice/CourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sen.courceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourceServiceApplication.class, args);
+	}
+
+}
---
+++ cource-service/src/test/java/com/sen/courceservice/CourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sen.courceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
