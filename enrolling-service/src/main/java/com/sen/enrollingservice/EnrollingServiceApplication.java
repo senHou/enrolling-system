07cc8ac4ad22f7573936d251226274cd9c67892e
shou <sen.hou1987@gamil.com>
2024-01-12 23:36:39 +1100

    add student service and enrolling service

---
+++ enrolling-service/src/main/java/com/sen/enrollingservice/EnrollingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sen.enrollingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollingServiceApplication.class, args);
+	}
+
+}
---
+++ enrolling-service/src/test/java/com/sen/enrollingservice/EnrollingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sen.enrollingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
